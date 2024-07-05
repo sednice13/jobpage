50159c10ab9439baca95c29846e7e44548969b84
Sednice13 <sa224pb@student.lnu.se>
2024-07-05 14:56:27 +0200

    adding all the JSX files needed for this project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Component } from 'react';
 
-function App() {
+
+class App extends Component {
+  render() {
+
+  
   return (
-    
+      <Router> 
+        <Routes>    
+            
+      </Routes> 
       <div className='fullsite'>
         <div className='centraltext'>
         <h2> Samuel Åhlander</h2>
         <h4> Click on my name to get started</h4>
         </div>
       </div>
+      </Router>
    
   );
+}
 }
 
 export default App;
