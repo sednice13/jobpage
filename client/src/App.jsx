a4f08a2f15fe828b8cafbfcf6d4745d332652396
Sednice13 <sa224pb@student.lnu.se>
2024-07-05 14:01:07 +0200

    central the text on welcome page

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+      <div className='fullsite'>
+        <div className='centraltext'>
+        <h2> Samuel Åhlander</h2>
+        <h4> Click on my name to get started</h4>
+        </div>
+      </div>
+   
+  );
+}
+
+export default App;
